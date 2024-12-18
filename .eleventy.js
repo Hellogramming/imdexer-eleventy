b7cad13b59d803b86c3b9b63127fb4da8674975f
TechAurelian <dev@techaurelian.com>
2024-12-18 13:39:24 +0200

    Refactor Eleventy plugin to improve image handling and update documentation

--- .eleventy.js
+++ .eleventy.js
@@ -6,29 +6,10 @@
 
 import { addImageShortcode } from "./src/image-shortcode.js";
 
-// /**
-//  * Validates the provided option by throwing an error if it is not defined.
-//  * 
-//  * @param {object} options The options object.
-//  * @param {string} name The name of the option.
-//  */
-// function validateOption(options, name) {
-//   if (!options[name]) throw new Error(`eleventy-mapped-images requires a ${name} option.`);
-// }
-
 export default function (eleventyConfig, options = {}) {
-  // Validate options
-  // validateOption(options, 'imageMaps');
+  // TODO: Validate options
 
   if (options.imgShortcode) {
     addImageShortcode(eleventyConfig, options.imgShortcode, options.zones);
   }
-
-  // The options object can contain one or more shortcodes to add in the shortcodes array
-  // Add the shortcodes to Eleventy
-  // if (options.shortcodes) {
-  //   for (const shortcode of options.shortcodes) {
-  //     addImageShortcode(eleventyConfig, shortcode.name, shortcode.imdexer, shortcode.baseUrl);
-  //   }
-  // }
 };
