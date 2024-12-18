a49a62a5a0910ab5feba200249b5da47c79a4165
TechAurelian <dev@techaurelian.com>
2024-12-18 13:08:08 +0200

    Update copyright information and author details across multiple files

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Appliberated (https://appliberated.com). All rights reserved.
+ * Copyright 2024 Hellogramming (https://www.hellogramming.com/). All rights reserved.
  * See LICENSE in the project root for license information.
  * @author TechAurelian <dev@techaurelian.com> (https://techaurelian.com)
  */
